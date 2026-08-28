6f9e1bd1080510c7331ce273894a1467a55c53e8
NivedhGovil <nivedh.govil@gmail.com>
2026-08-28 11:52:49 +0530

    Create Caesar cipher.py

---
+++ Caesar cipher.py
@@ -0,0 +1,14 @@
+word= str(input("Enter a four letter word:"))
+a= word[0]
+b= word[1]
+c= word[2]
+d= word[3]
+a1=ord(a)
+b1=ord(b)
+c1=ord(c)
+d1=ord(d)
+fa1=((ord(a) +1))
+fb1=((ord(b) +1))
+fc1=((ord(c) +1))
+fd1=((ord(d) +1))
+print(chr(fa1) + chr(fb1) + chr(fc1) + chr(fd1))
